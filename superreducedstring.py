47fbac861ca565bb66e6f1a221196e50e1cfc023
Chirag Chaplot <chiragchaplot@gmail.com>
2017-02-26 23:45:06 +0300

    superreducedstring.py
    problem superreducedstring.py

---
+++ superreducedstring.py
@@ -0,0 +1,15 @@
+import math
+import re
+#This is a solution of the hackerrank problem.
+def reduce(s):
+    temp = s
+
+    if (type(temp) == 'String'):
+#Proceed if type of entry is string
+        #Traverse through string
+
+    pass
+
+
+reduce()
+
